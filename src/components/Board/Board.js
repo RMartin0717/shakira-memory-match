20dd86a22ad6e7030d4b9b2d62799885902a0863
Riley Martin <riley.willow@gmail.com>
2021-06-29 16:46:53 -0600

    Board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -18,10 +18,10 @@ class Board extends Component {
   }
 
   shuffle(array) {
-      for (let i = 0; i < array.length - 1; i++) {
-        const j = Math.floor(Math.random() * (array.length));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
+    for (let i = 0; i < array.length - 1; i++) {
+      const j = Math.floor(Math.random() * (array.length));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
   }
 
   render() {
