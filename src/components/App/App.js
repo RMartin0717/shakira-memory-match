86ea01bff1f1cf4b1a7abba65263094c3297f1c8
Riley Martin <riley.willow@gmail.com>
2021-08-17 12:06:03 -0600

    Implement hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from "react"
+import React from "react"
 import Board from "../Board/Board"
 import "./App.css"
 
-class App extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <main>
-        <h1>Shakira Memory Match</h1>
-        <h3>Click on a card to see its picture, then try to guess where its match is and click on it!</h3>
-        <Board />
-      </main>
-    )
-  }
+const App = () => {
+  return (
+    <main>
+      <h1>Shakira Memory Match</h1>
+      <h3>Click on a card to see its picture, then try to guess where its match is and click on it!</h3>
+      <Board />
+    </main>
+  )
 }
 
 export default App
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { useState, useEffect } from "react"
 import Card from "../Card/Card"
 import "./Board.css"
 import shakiraOne from "../../assets/shakira1.jpeg"
@@ -9,38 +9,57 @@ import shakiraFive from "../../assets/shakira5.jpeg"
 import shakiraSix from "../../assets/shakira6.jpeg"
 
 
-class Board extends Component {
-  constructor() {
-    super();
-    this.state = {
-      cards: [{id: 1, image: shakiraOne}, {id: 1, image: shakiraOne}, {id: 2, image: shakiraTwo}, {id: 2, image: shakiraTwo}, {id: 3, image: shakiraThree}, {id: 3, image: shakiraThree}, {id: 4, image: shakiraFour}, {id: 4, image: shakiraFour}, {id: 5, image: shakiraFive}, {id: 5, image: shakiraFive}, {id: 6, image: shakiraSix}, {id: 6, image: shakiraSix}]
-    }
-  }
+const Board = () => {
+  const [cards, setCards] = useState([
+    {id: 1, image: shakiraOne},
+    {id: 1, image: shakiraOne},
+    {id: 2, image: shakiraTwo},
+    {id: 2, image: shakiraTwo},
+    {id: 3, image: shakiraThree},
+    {id: 3, image: shakiraThree},
+    {id: 4, image: shakiraFour},
+    {id: 4, image: shakiraFour},
+    {id: 5, image: shakiraFive},
+    {id: 5, image: shakiraFive},
+    {id: 6, image: shakiraSix},
+    {id: 6, image: shakiraSix}
+  ])
+  //refactor this to start with one of each image, then use spread operator to double the array?
 
-  shuffle(array) {
+  useEffect(() => {
+    console.log(cards)
+    const shuffledCards = shuffle(cards)
+    console.log(shuffledCards)
+    setCards(shuffledCards)
+  })
+
+  const shuffle = (array) => {
     for (let i = 0; i < array.length - 1; i++) {
       const j = Math.floor(Math.random() * (array.length));
       [array[i], array[j]] = [array[j], array[i]];
     }
+    return array
   }
 
-  render() {
-    this.setState(this.shuffle(this.state.cards))
-
-    const makeCards = this.state.cards.map(card => {
+  const allCards = () => {
+    const makeCards = cards.map(card => {
       return (
         <Card
-          key = {card.id}
-          id = {card.id}
-          image = {card.image}
+        key = {card.id}
+        id = {card.id}
+        image = {card.image}
         />
       )
     })
-
-    return (
-      <section>{makeCards}</section>
-    )
+    return makeCards
   }
+
+  return (
+    <>
+      <section>{allCards}</section>
+      <button onClick={shuffle}>New Game</button>
+    </>
+  )
 }
 
 export default Board
